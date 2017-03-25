692b1bdab89fdd23996db62d63b985d956a0ed61
Felicia Agatha <agatha.flc@hotmail.com>
2017-03-25 17:50:38 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 25, 2017
+ *      Author: Felicia Agatha
+ */
+
+#include <iostream>
+using namespace std;
+
+const string WELCOME_MESSAGE = "Welcome to the Battle Simulator.\nPlease input the following information:";
+
+int main() {
+	return 0;
+}
+
+
